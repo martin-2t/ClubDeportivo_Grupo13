f586a3e184f3b62b4a0cbfa7f990999526350729
martin-2t <martins495728@gmail.com>
2025-10-02 13:15:39 -0300

    Aplicar .gitignore y limpiar archivos ignorados

---
+++ Datos/conexión.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace ClubDeportivo.Datos
+{
+    internal class conexión
+    {
+        
+        private string cadena = "server=localhost;port=3306;database=bd_club;uid=root;pwd=Rojo123;";
+
+        
+        public MySqlConnection GetConnection()
+        {
+            return new MySqlConnection(cadena);
+        }
+    }
+}
+
