796497e25b63e82f9fd7ade86d3b6a3a953002a4
Emanuel Grigolatto <ema_grigo@hotmail.com>
2025-10-04 00:34:30 -0300

    se agrego lo referido al login y la conexion la base de datos

---
+++ Datos/Conexion.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace ClubDeportivo.Datos
+{
+    internal class Conexion
+    {
+
+        private string baseDatos;
+        private string servidor;
+        private string puerto;
+        private string usuario;
+        private string clave;
+        private static Conexion con = null;
+
+        
+
+        private Conexion()
+        {
+            this.baseDatos = "Proyecto";
+            this.servidor = "localhost";
+            this.puerto = "3306";
+            this.usuario = "root";
+            this.clave = Environment.GetEnvironmentVariable("DB_PASSWORD");
+        }
+
+        public MySqlConnection CrearConcexion()
+        {
+            MySqlConnection cadena = new MySqlConnection();
+
+            try
+            {
+                cadena.ConnectionString = "datasource=" + this.servidor +
+                                          ";port=" + this.puerto +
+                                          ";username=" + this.usuario +
+                                          ";password=" + this.clave +
+                                          ";Database=" + this.baseDatos;
+            }
+            catch (Exception ex)
+            {
+                cadena = null;
+                throw;
+            }
+            return cadena;
+        }
+
+        public static Conexion getInstancia()
+        {
+            if (con == null) // quiere decir que la conexion esta cerrada
+            {
+                con = new Conexion(); // se crea una nueva
+            }
+            return con;
+        }
+    }
+}
+
+
